c92b2a74448f24906844b234414858764f7fd47b
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-09-15 16:52:26 -0400

    dummy data rendering

--- client/AllAlbums.js
+++ client/AllAlbums.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
 const AllAlbums = (props) => {
+    const albums = props.albums
     return (
         <div className='container'>
-            <div id='albums' className='row wrap'>
-                <div className='album'>
+            <div id='albums' className='row '>
+                <div className='album row'>
+                    {albums.map(album => {
+                        return (
+                            
+                                <div className='album'>
+                                    <img src={album.artworkUrl} />
+                                    <p>{album.name}</p>
+                                    <small>{album.artist.name}</small>
+                                </div>
+                        )
 
-                    <img src='default-album.jpg' />
+
+                    })
+                    }
 
                 </div>
             </div>
--- client/Main.js
+++ client/Main.js
@@ -4,12 +4,40 @@ import Player from './Player'
 import AllAlbums from './AllAlbums'
 
 export default class Main extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      albums : [
+        {
+          "id": 1,
+          "name": "No Dummy",
+          "artworkUrl": "default-album.jpg",
+          "artistId": 1,
+          "artist": {
+            "id": 1,
+            "name": "The Crash Test Dummies"
+          }
+        },
+        {
+          "id": 2,
+          "name": "I React to State",
+          "artworkUrl": "default-album.jpg",
+          "artistId": 1,
+          "artist": {
+            "id": 1,
+            "name": "The Crash Test Dummies"
+          }
+        }
+      
+      ]
+    }
+  }
   render () {
     return (
       <div id='main' className='row container'>
         <SideBar/>
         <Player />
-        <AllAlbums />
+        <AllAlbums albums = {this.state.albums}/>
       </div>
     )
   }
