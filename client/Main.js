b9e0e573f476df04f8d9bbda018f5a8f585fa3fe
Nuria Rodriguez <nurialuzr1@gmail.com>
2020-09-15 15:38:40 -0400

    added PlayerBar on the bottom of the page

--- client/Main.js
+++ client/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SideBar from './SideBar'
+import Player from './Player'
 
 export default class Main extends React.Component {
   render () {
     return (
       <div id='main' className='row container'>
         <SideBar/>
+        <Player />
       </div>
     )
   }
